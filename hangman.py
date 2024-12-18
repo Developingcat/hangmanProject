9b304e98e1fe2f14dcb10dde7ea53f9b31f92f87
Hannah <148793520+Developingcat@users.noreply.github.com>
2024-12-18 14:44:49 +0000

    Worked more on the hangman project

--- hangman.py
+++ hangman.py
@@ -112,33 +112,46 @@ def main():
         foundAllLetters = True # Start off assuming they've won, for 
         for secretWordLetter in secretWord: 
             if secretWordLetter not in correctLetters: 
-            # There's a letter in the secret word that isn't yet in correctLetters, so the player hasn't won the game yet.
-            foundAllLetters = False
+                # There's a letter in the secret word that isn't yet in correctLetters, so the player hasn't won the game yet.
+                foundAllLetters = False
+                break
+            if foundAllLetters: 
+                print("Yes! The secret word is:", secretWord)
+                print("You have won.")
+                break # break out of the main gameloop 
+        else: 
+            # The player has guessed incorrectly:
+            missedLetters.append(guess)
+        # check if player has guessed too many times and lost, the "-1" is because we don't count the empty gallows in HANGMAN_PICS 
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
+            drawHangman(missedLetters, correctLetters, secretWord)
+            print("You have run out of guesses.")
+            print('The word was "{}"'.format(secretWord))
             break
-        if foundAllLetters: 
-            print("Yes! The secret word is:", secretWord)
-            print("You have won.")
-            break # break out of the main gameloop 
-
 # Draw current state of the hangman game, along with the missed and correctly guessed letters of the secret word. 
 
 def drawHangman(missedLetters, correctLetters, secretWord):
-    print(HANGMAN_PICS[len(misssedLetters)])
+    print(HANGMAN_PICS[len(missedLetters)])
     print('The category selected is: ', CATEGORY)
     print()
 
 
-# Show the incorrectly guessed letters 
+    # Show the incorrectly guessed letters 
+    print('Missed Letters: ', end='')
+    for letter in missedLetters:
+        print(letter, end=' ')
+    if len(missedLetters) == 0:
+        print("No missed letters yet.")
+    print()
+    
 
+    # Display the blanks for the secret word (one blank per letter)
+    blanks = ['_'] * len(secretWord)
+    # Replace blanks with correctly guessed letters:
 
+    #Show the secret word with spaces in between each letter 
 
-# Display the blanks for the secret word (one blank per letter)
-blanks = ['_'] * len(secretWord)
-# Replace blanks with correctly guessed letters:
-
-#Show the secret word with spaces in between each letter 
-
-# Returns the letter the player entered, this function makes sure the player entered a single letter that they haven't guessed before. 
+    # Returns the letter the player entered, this function makes sure the player entered a single letter that they haven't guessed before. 
 
 def getPlayerGuess(alreadyGuessed): 
     while True: # This will keep asking until the player enters a valid letter. 
